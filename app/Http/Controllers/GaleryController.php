c8af922844861ad65fb2b87bd9bd8d39a4475acb
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-11-04 01:53:34 +0700

    Prepare Database

---
+++ app/Http/Controllers/GaleryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Galery;
+use Illuminate\Http\Request;
+
+class GaleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Galery  $galery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Galery $galery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Galery  $galery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Galery $galery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Galery  $galery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Galery $galery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Galery  $galery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Galery $galery)
+    {
+        //
+    }
+}
