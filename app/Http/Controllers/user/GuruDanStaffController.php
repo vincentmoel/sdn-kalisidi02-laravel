3491c7e89c9e6cbf905027b6a3c6fcb2370b984f
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-12-06 10:22:56 +0700

    index Guru for User

--- app/Http/Controllers/user/GuruDanStaffController.php
+++ app/Http/Controllers/user/GuruDanStaffController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
-
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 
 class GuruDanStaffController extends Controller
 {
     public function index()
     {
-        return view('user.Guru-Staff.index');
+        return view('user.Guru-Staff.index',[
+            'principals' => Teacher::where('position_id','=','1')->get(),
+            'teachers' => Teacher::where('position_id','!=','1')->orderBy('position_id','asc')->get(),
+        ]);
     }
+
 }
--- resources/views/user/Guru-Staff/index.blade.php
+++ resources/views/user/Guru-Staff/index.blade.php
@@ -10,6 +10,7 @@
         height: 196px;
         object-fit: cover;
     }
+
 </style>
 
 @extends('layout.user.main',['title'=>'Guru dan Staff'])
@@ -17,7 +18,8 @@
 
 @section('container')
     <div style="margin-top: 84px"></div>
-    <div class="container-fluid text-light py-5" style="background-image: linear-gradient(to right top, #0081c0, #0098cb, #00adc5, #00bfb0, #00ce91);">
+    <div class="container-fluid text-light py-5"
+        style="background-image: linear-gradient(to right top, #0081c0, #0098cb, #00adc5, #00bfb0, #00ce91);">
         <div class="row">
             <div class="col-12 text-center">
                 <h1>Guru dan Staff</h1>
@@ -25,104 +27,44 @@
         </div>
     </div>
     <div style="margin-top: 40px"></div>
-    <div class="container" style="background-image: linear-gradient(to right bottom, #f8eed8, #e8ead4, #d8e6d3, #c9e2d4, #bddcd6); border-radius: 10px;">
+    <div class="container"
+        style="background-image: linear-gradient(to right bottom, #f8eed8, #e8ead4, #d8e6d3, #c9e2d4, #bddcd6); border-radius: 10px;">
         <div class="row justify-content-center py-4">
-            
+
             <div class="col-lg-10 col-11">
                 <div class="row justify-content-center my-4">
-                    <div class="col-lg-3 col-6">
-                        <div class="card border-dark pict-kepsek">
-                            <img src="/images/gurustaff-ex.png" class="card-img-top" alt="">
-                            <div class="card-body text-center">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
+                    @foreach ($principals as $principal)
+                        <div class="col-lg-3 col-6">
+                            <div class="card border-dark pict-kepsek">
+                                <img src="{{ asset('images/uploads/' . $principal->image) }}" class="card-img-top" alt="">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">{{ $principal->name }}</h5>
+                                    <p class="mb-0">{{ $principal->position->name }}</p>
+                                    <p class="mb-0">{{ $principal->nip }}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
+
                 </div>
                 <div class="row justify-content-center row-cols-lg-5 row-cols-2 g-4 my-4">
+                    @foreach ($teachers as $teacher)
+                        
                     <div class="col">
                         <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
+                            <img src="{{ asset('images/uploads/' . $teacher->image) }}" class="img-card-top" alt="">
                             <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
+                                <h5 class="card-title">{{ $teacher->name }}</h5>
+                                <p class="mb-0">{{ $teacher->position->name }}</p>
+                                <p class="mb-0">{{ $teacher->nip }}</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card border-dark pict-guru-staff">
-                            <img src="/images/gurustaff-ex.png" class="img-card-top" alt="">
-                            <div class="card-body text-center   ">
-                                <h5 class="card-title">nama</h5>
-                                <p class="mb-0">jabatan</p>
-                                <p class="mb-0">nip</p>
-                            </div>
-                        </div>
-                    </div>
-                    
+                    @endforeach
+
+
+
+
                 </div>
             </div>
         </div>
