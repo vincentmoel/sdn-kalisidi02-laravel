eba943185fd770ec2a71a87181e14180279d8e07
fadhelaps <aqiel727@gmail.com>
2021-12-08 21:24:24 +0700

    show-berita

--- resources/views/user/Berita/show.blade.php
+++ resources/views/user/Berita/show.blade.php
@@ -1,8 +1,38 @@
 @extends('layout.user.main',['title'=>'Show'])
 
-
-
 @section('container')
+    <div style="margin-top: 84px"></div>
+    <div class="container-fluid py-5"
+        style="background-image: linear-gradient(to right top, #0081c0, #0098cb, #00adc5, #00bfb0, #00ce91)">
+        <div class="row">
+            <div class="col-12 text-center text-light">
+                <h1>Berita Sekolah</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid py-5" style="background-color: #EFEFEF">
+        <div class="container w-75 mx-auto bg-white">
+            <div class="row pt-4 justify-content-center">
+                <div class="col-11">
+                    <h1>ini judul berita ini judul berita</h1>
+                </div>
+                <div class="col-11">
+                    <p class="text-muted" style="font-size: 14px">dd/mm/yyyy</p>
+                </div>
+                <hr style="border: solid #1277cb">
+            </div>
+
+            <div class="row pb-4 justify-content-center">
+                <div class="col-11 text-center pt-2 pb-4">
+                    <img src="/images/gurustaff-ex.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-11">
+                    <p style="font-size: 16px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada orci vitae magna fermentum sodales. Integer quis tincidunt justo, nec ullamcorper ligula. Nulla ullamcorper lobortis purus a tristique. Mauris vehicula odio dictum odio posuere, eget pulvinar libero dignissim. Donec pellentesque iaculis hendrerit. Integer ultricies placerat consequat. Etiam condimentum dui et lectus auctor, et commodo leo vestibulum. Curabitur maximus ante tortor, at lobortis sem pulvinar a. Praesent ut urna sit amet nulla venenatis ullamcorper a id erat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
